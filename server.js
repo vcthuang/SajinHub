7e1ac011cc0f145fe7259c853ac928a468c6c5dc
Val <57106675+vcthuang@users.noreply.github.com>
2020-03-16 19:58:34 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,47 @@
+// Include import libraries to be used
+// express for routing
+const express = require('express');
+
+// express for database
+const mongoose = require('mongoose');
+
+// bodyPaser for parsing html
+const bodyParser = require('body-parser');
+
+// passport for token authentication
+const passport = require('passport');
+
+// Include our own files 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+// start with initialization
+// create an instance of our app
+const app = express();
+
+// body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// connect to mongodb
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDb connected!'))
+  .catch(err => console.log(err));
+
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require('./config/passport')(passport)
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+const port = 8020;
+app.listen(port, () => console.log(`Server running on port ${port}`) );
