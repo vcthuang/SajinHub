3af11ff0aaaea77efde21b29c835c933b951ebc0
Val <57106675+vcthuang@users.noreply.github.com>
2020-04-27 15:23:58 -0700

    Change UserPosts

--- client/src/components/posts/UserPosts.js
+++ client/src/components/posts/UserPosts.js
@@ -2,61 +2,30 @@
 // This is to be displayed under user profile
 
 import React, { Component } from 'react';
-
 import PostFeed from './PostFeed';
-
-import { getUserPosts } from '../../actions/postActions'
+import PropTypes from 'prop-types';
 
 class UserPosts extends Component {
 
-  componentDidMount() {
-    if (this.props.match.params.id) {
-      this.props.getUserPosts(this.props.match.params.id);
-    }
-  }
-
-  render() {
-
-    const { userposts, loading } = this.props.post;
- 
-    let postContent;
-
-    if (loading || userposts === []) {
-      postContent = <Spinner />
-
-    } else {
-      postContent = (
-        <div className="card-body justify-content-center">
-          <h5 className="card-header">
-            <i className="fas fa-edit pr-2" style={{ color: "red" }}></i>
-          Recent Posts
-          </h5>
-          <PostFeed posts={userposts} />
-        </div>
-      );
-    }
-  
+  render() {     
     return (     
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-xs-12 col-sm-12 col-md-9 col-lg-7 col-xl-6 align-self-center">
-            <br />
-            {postContent}
-          </div>
+      
+        <div className="card ">
+          <h5 className="card-header">      
+            <i className="fas fa-edit pr-2" style={{ color: "red" }}></i>
+              Recent Posts
+            </h5>
+            <div className="card-body justify-content-center">
+              <PostFeed posts={this.props.posts} />
+            </div>
         </div>
-      </div>
-
+  
     )
   }
 } 
 
 UserPosts.propTypes = {
   post: PropTypes.object.isRequired,
-  getUserPosts: PropTypes.func.isRequired
 }
 
-const mapStateToProps = state => ({
-  post: state.post
-});
-
-export default connect(mapStateToProps, { getUserPosts })(UserPosts);
+export default UserPosts;
