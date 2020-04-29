b3942ad773808e19d407c8470b1bf91d7cc73223
Shina Park <shina820@gmail.com>
2020-04-29 11:33:58 -0700

    Prepared to deploy

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ if (process.env.NODE_ENV === 'production') {
 
   // default 'index,js'
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.js'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
 }
 
